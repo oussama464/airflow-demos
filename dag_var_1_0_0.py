55e3a8253d1d555e48ed244d9a16c16bc2c9e6e0
oussama464 <oussama.missaoui201@gmail.com>
2022-12-29 21:46:48 +0100

    retries

--- dag_var_1_0_0.py
+++ dag_var_1_0_0.py
@@ -1,8 +1,7 @@
-from multiprocessing.connection import wait
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
-from datetime import datetime
+from datetime import datetime, timedelta
 
 DEFAULT_ARGS = {"owner": "airflow"}
 
@@ -17,16 +16,19 @@ with DAG(
     start_date=datetime(2022, 12, 12),
     default_args=DEFAULT_ARGS,
     schedule_interval="@daily",
-    dagrun_timeout=40,
-    catchup=True,
+    catchup=False,
 ) as dag:
     task_a = BashOperator(
         task_id="task_a",
-        bash_command="echo 'task_a' && sleep 10",
-        # wait_for_downstream=True,
+        bash_command="echo 'task_a' && sleep 6",
     )
     task_b = BashOperator(
-        task_id="task_b", bash_command="echo 'task_b' && exit 0", task_concurrency=1
+        task_id="task_b",
+        bash_command="echo 'task_b' && sleep 6 && exit 1",
+        retries=3,
+        email=["ouss.miss@gmail"],
+        email_on_retry=False,
+        retry_delay=timedelta(seconds=5),
     )
     task_c = PythonOperator(
         task_id="task_c", python_callable=_my_func, depends_on_past=True
